a6797a931f7dce77c3c314e23b43ec076a39aa2d
moxDroid Labs Inc <pritam.world@gmail.com>
2019-03-06 18:23:44 -0500

    Login and Home View Controller added

---
+++ Day3SegueAndNavigationVCExample/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Day3SegueAndNavigationVCExample
+//
+//  Created by moxDroid on 2019-03-06.
+//  Copyright © 2019 moxDroid. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var swRememberMe: UISwitch!
+    @IBOutlet weak var txtUserPassword: UITextField!
+    @IBOutlet weak var txtUserEmail: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+    @IBAction func btnLoginClick(_ sender: Any)
+    {
+    }
+}
+
